606cd75b5c963156cc3ddd8423cd5574f8061200
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-28 21:31:48 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -5,7 +5,7 @@ const dbURI = "mongodb+srv://ajifu917:ajifu917@atlascluster.sws1np9.mongodb.net/
 function connectMongoDb() {
   mongoose.connect(dbURI, {
     serverApi: { version: '1', strict: true, deprecationErrors: true },
-    serverSelectionTimeoutMS: 10000 // menambahkan timeout
+    serverSelectionTimeoutMS: 8000 // menambahkan timeout
   })
   .then(() => {
     console.log(color('[INFO] Connect to DB success!', 'red'));
