df1724c3c888cca936358f0877ed0e7ed3be7081
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-28 21:00:33 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 const { color } = require('../lib/color.js');
-const { dbURI } = require('../lib/settings');
+const dbURI = "mongodb+srv://ajiart01:ajifu917@atlascluster.sws1np9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 function connectMongoDb() {
-   mongoose.connect(dbURI, { 
-      useNewUrlParser: true, 
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000 // menambahkan timeout
-    })
-    .then(() => {
-      console.log(color('[INFO] Connect to DB success!','red'));
-    })
-    .catch(err => {
-      console.error(color(`[ERROR] Failed to connect to DB: ${err.message}`, 'red'));
-    });
-};
+  mongoose.connect(dbURI, {
+    serverApi: { version: '1', strict: true, deprecationErrors: true },
+    serverSelectionTimeoutMS: 5000 // menambahkan timeout
+  })
+  .then(() => {
+    console.log(color('[INFO] Connect to DB success!', 'red'));
+  })
+  .catch(err => {
+    console.error(color(`[ERROR] Failed to connect to DB: ${err.message}`, 'red'));
+  });
+}
 
 module.exports.connectMongoDb = connectMongoDb;
