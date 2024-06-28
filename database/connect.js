7eb978281d9331ec2f8dac25abe8035c73ab8996
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-28 21:45:30 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 const { color } = require('../lib/color.js');
-const dbURI = "mongodb+srv://marinkitagawa:johannes123@restapi.pbjm0nh.mongodb.net/?retryWrites=true&w=majority";
+const { dbURI } = require('../lib/settings');
 
 function connectMongoDb() {
-  mongoose.connect(dbURI, {
-    serverApi: { version: '1', strict: true, deprecationErrors: true },
-    serverSelectionTimeoutMS: 8000 // menambahkan timeout
-  })
-  .then(() => {
-    console.log(color('[INFO] Connect to DB success!', 'red'));
-  })
-  .catch(err => {
-    console.error(color(`[ERROR] Failed to connect to DB: ${err.message}`, 'red'));
-  });
-}
+   mongoose.connect(dbURI, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.on('open', () => {
+      console.log(color('[INFO] Connect to DB success!','red'));
+    });
+};
 
 module.exports.connectMongoDb = connectMongoDb;
