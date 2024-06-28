915ce257cfd4b2fd365bc0d7b7b2d723287e7110
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-29 02:31:37 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
-const mongodb = require('mongodb');
 const { color } = require('../lib/color.js');
 const { dbURI } = require('../lib/settings');
 
 function connectMongoDb() {
-   mongoose.connect(dbURI, { 
-      useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000, // Waktu tunggu koneksi
-  socketTimeoutMS: 45000, // Waktu tunggu soket
-});
-      useCreateIndex: true
-    });
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.on('open', () => {
-      console.log(color('[INFO] Connect to DB success!','red'));
-    });
-};
+  mongoose.connect(dbURI, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Waktu tunggu koneksi
+    socketTimeoutMS: 45000, // Waktu tunggu soket
+    useCreateIndex: true // Pastikan ini berada di dalam objek konfigurasi
+  }).then(() => {
+    console.log(color('[INFO] Connect to DB success!', 'red'));
+  }).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+  
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+}
 
 module.exports.connectMongoDb = connectMongoDb;
