730777437241ce3224f752c7facba28475257f3f
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-29 14:44:43 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -4,23 +4,26 @@ const { color } = require('../lib/color.js');
 // URL koneksi MongoDB Atlas yang diperbarui
 const uri = 'mongodb+srv://ajifu917:ajifu917@atlascluster.sws1np9.mongodb.net/myDatabase?retryWrites=true&w=majority';
 
-function connectMongoDb() {
-    await mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  bufferCommands: false
-});
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('Connection error: ' + err.message));
+async function connectMongoDb() {
+    try {
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            bufferCommands: false
+        });
 
+        console.log('MongoDB connected');
 
-    const db = mongoose.connection;
-    db.on('error', (error) => {
-        console.error('Connection error:', error);
-    });
-    db.once('open', () => {
-        console.log('Database connected');
-    });
+        const db = mongoose.connection;
+        db.on('error', (error) => {
+            console.error('Connection error:', error);
+        });
+        db.once('open', () => {
+            console.log('Database connected');
+        });
+    } catch (error) {
+        console.log('Connection error: ' + error.message);
+    }
 }
 
 module.exports.connectMongoDb = connectMongoDb;
