fa232dd5e9fcbcc28d9e9a1b0da56480de7e31d7
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-29 13:20:04 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
+const mongodb = require('mongodb');
 const { color } = require('../lib/color.js');
 //const { dbURI } = require('../lib/settings');
-
 const uri = 'mongodb+srv://ajifu917:ajifu917@atlascluster.sws1np9.mongodb.net/?retryWrites=true&w=majority';
+
 function connectMongoDb() {
-  mongoose.connectMongoDb(uri).then(() => {
-    console.log(color('[INFO] Connect to DB success!', 'red'));
-  }).catch(err => {
-    console.error('Failed to connect to MongoDB', err);
-  });
-  
-  const db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'connection error:'));
-}
+   mongoose.connect(uri, { 
+    });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.on('open', () => {
+      console.log(color('[INFO] Connect to DB success!','red'));
+    });
+};
 
 module.exports.connectMongoDb = connectMongoDb;
