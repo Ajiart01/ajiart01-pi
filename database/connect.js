0dc490713107d10fe1b7eacf5997d9b251955b67
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-29 13:26:53 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,17 +1,24 @@
 const mongoose = require('mongoose');
-const mongodb = require('mongodb');
 const { color } = require('../lib/color.js');
-//const { dbURI } = require('../lib/settings');
+// const { dbURI } = require('../lib/settings');
 const uri = 'mongodb+srv://ajifu917:ajifu917@atlascluster.sws1np9.mongodb.net/?retryWrites=true&w=majority';
 
 function connectMongoDb() {
    mongoose.connect(uri, { 
-    });
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.on('open', () => {
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+       serverSelectionTimeoutMS: 30000, // Tambahkan ini untuk mengatur timeout koneksi
+       writeConcern: {
+           w: 'majority',
+           wtimeout: 5000,
+       },
+   });
+   
+   const db = mongoose.connection;
+   db.on('error', console.error.bind(console, 'connection error:'));
+   db.once('open', () => {
       console.log(color('[INFO] Connect to DB success!','red'));
-    });
+   });
 };
 
 module.exports.connectMongoDb = connectMongoDb;
