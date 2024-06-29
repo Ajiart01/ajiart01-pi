27b2dd22c055af5e9efc259e4e9171815fe2c73c
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-29 13:51:08 +0700

    Update model.js

--- database/model.js
+++ database/model.js
@@ -1,31 +1,34 @@
 const mongoose = require('mongoose');
 const { expiredTokenDb } = require('../lib/settings');
 
-const Users = mongoose.Schema({
-    username: { type: String },
-    password: { type: String},
-    apikey: { type: String },
-    defaultKey: { type: String },
-    premium: { type: String },
-    limit: { type: Number },
-    email: { type: String },
-}, { versionKey: false });
-module.exports.User = mongoose.model('api2', Users);
+// Skema Users
+const UsersSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    apikey: { type: String, required: true, unique: true },
+    defaultKey: { type: String, required: true },
+    premium: { type: Date, default: null },
+    limit: { type: Number, default: limitCount },
+    email: { type: String, required: true, unique: true },
+}, { versionKey: false, timestamps: true });
+module.exports.User = mongoose.model('User', UsersSchema);
 
-const Utils = mongoose.Schema({
-    total: { type: Number },
-    today: { type: Number },
-    visitor: { type: Number },
-    util: { type: String }
+// Skema Utils
+const UtilsSchema = new mongoose.Schema({
+    total: { type: Number, default: 0 },
+    today: { type: Number, default: 0 },
+    visitor: { type: Number, default: 0 },
+    util: { type: String, default: 'util', unique: true },
 }, { versionKey: false });
-module.exports.Utils = mongoose.model('util', Utils);
+module.exports.Utils = mongoose.model('Util', UtilsSchema);
 
-const Token = mongoose.Schema({
-    token: { type: String },
+// Skema Token
+const TokenSchema = new mongoose.Schema({
+    token: { type: String, required: true, unique: true },
     expire_at: {
         type: Date,
         default: Date.now,
         expires: expiredTokenDb,
     }
 }, { versionKey: false });
-module.exports.Token = mongoose.model('token', Token);
+module.exports.Token = mongoose.model('Token', TokenSchema);
