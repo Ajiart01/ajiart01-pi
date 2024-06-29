7575e8d66da0a4b193dae556cb7117158fd46565
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-29 13:38:32 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,28 +1,34 @@
 const mongoose = require('mongoose');
 const { color } = require('../lib/color.js');
 
-// Perbarui URL koneksi MongoDB Atlas dengan yang direkomendasikan
+// URL koneksi MongoDB Atlas yang diperbarui
 const uri = 'mongodb+srv://ajifu917:ajifu917@atlascluster.sws1np9.mongodb.net/myDatabase?retryWrites=true&w=majority';
 
 function connectMongoDb() {
-   mongoose.connect(uri, { 
-       useNewUrlParser: true,
-       useUnifiedTopology: true,
-       serverSelectionTimeoutMS: 30000, // Sesuaikan timeout koneksi
-       writeConcern: {
-           w: 'majority',
-           wtimeout: 5000,
-       },
-   })
-   .then(() => {
-       console.log(color('[INFO] Connect to DB success!','red'));
-   })
-   .catch((error) => {
-       console.error('Connection error:', error);
-   });
+    mongoose.connect(uri, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000, // Tingkatkan timeout koneksi
+        socketTimeoutMS: 45000,         // Menambahkan timeout soket
+        writeConcern: {
+            w: 'majority',
+            wtimeout: 5000,
+        },
+    })
+    .then(() => {
+        console.log(color('[INFO] Connect to DB success!','red'));
+    })
+    .catch((error) => {
+        console.error('Connection error:', error);
+    });
 
-   const db = mongoose.connection;
-   db.on('error', console.error.bind(console, 'connection error:'));
-};
+    const db = mongoose.connection;
+    db.on('error', (error) => {
+        console.error('Connection error:', error);
+    });
+    db.once('open', () => {
+        console.log('Database connected');
+    });
+}
 
 module.exports.connectMongoDb = connectMongoDb;
